05ebde1f1b608dcfe1f7f4b08c1d3b2f6ecd0572
Maruf-dev <maruf123erkinov@gmail.com>
2023-07-13 12:31:54 +0500

    tryed to change alert to seft closing

--- index.js
+++ index.js
@@ -1,86 +1,79 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue,remove} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+  remove,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
-    databaseURL:  "https://playground-b8997-default-rtdb.asia-southeast1.firebasedatabase.app/"
-}
+  databaseURL:
+    "https://playground-b8997-default-rtdb.asia-southeast1.firebasedatabase.app/",
+};
 
-
-const app = initializeApp(appSettings)
-const database = getDatabase(app)
-const shoppingListInDB = ref(database, "shoppingList")
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList");
 
 const inputFieldEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
 const shoppingListEl = document.getElementById("shopping-list");
+const alertBox = document.getElementsByClassName("alert");
 
+addButtonEl.addEventListener("click", function () {
+  let inputValue = inputFieldEl.value;
 
+  if (inputValue === "" || inputValue === " ") {
+    alert(" BO'SH jo'natib bo'lmaydi ");
 
-addButtonEl.addEventListener("click", function(){
-    let inputValue = inputFieldEl.value
+    setTimeout(function () {
+      alertBox.style.display = "none";
+    }, 5000);
+  } else {
+    push(shoppingListInDB, inputValue);
 
-    if (inputValue === "" || inputValue === " "){
+    clearInputFieldEl();
+  }
+});
 
-        alert("bo'sh habar jo'natib bo'lmaydi");
+onValue(shoppingListInDB, function (snapshot) {
+  if (snapshot.exists()) {
+    let itemsArray = Object.entries(snapshot.val());
 
-    }else{
+    clearShoppingListEl();
 
-        push(shoppingListInDB, inputValue);
-
-        clearInputFieldEl();
-
+    for (let i = 0; i < itemsArray.length; i++) {
+      let currentItem = itemsArray[i];
+      let currentItemId = currentItem[0];
+      let currentItemValue = currentItem[1];
+      appendItemTOShoppingListEl(currentItem);
     }
-    
+  } else {
+    shoppingListEl.innerHTML = "SMS yozsa ham bo'ladi...";
+  }
+});
 
-})
-
-onValue(shoppingListInDB, function(snapshot){
-
-    if (snapshot.exists()){
-        let itemsArray = Object.entries(snapshot.val())
-        
-        clearShoppingListEl()
-    
-        for (let i = 0; i < itemsArray.length; i++){
-            let currentItem = itemsArray[i]
-            let currentItemId = currentItem[0]
-            let currentItemValue = currentItem[1]
-            appendItemTOShoppingListEl(currentItem)
-        }
-
-    } else{
-        shoppingListEl.innerHTML = "SMS yozsa ham bo'ladi..."
-
-    }
-
-    
-})
-
-
-
-
-function clearShoppingListEl(){
-    shoppingListEl.innerHTML = ""
-}
-              
-function clearInputFieldEl(){
-    inputFieldEl.value = "";
-
+function clearShoppingListEl() {
+  shoppingListEl.innerHTML = "";
 }
 
-function appendItemTOShoppingListEl(item){
-    let itemId = item[0]
-    let itemValue = item[1]
+function clearInputFieldEl() {
+  inputFieldEl.value = "";
+}
 
-    let newEl = document.createElement('li')
+function appendItemTOShoppingListEl(item) {
+  let itemId = item[0];
+  let itemValue = item[1];
 
-    newEl.textContent = itemValue
+  let newEl = document.createElement("li");
 
-    newEl.addEventListener('click', function(){
-        let exactLocationOfItemInDB = ref(database,`shoppingList/${itemId}`)
-        remove(exactLocationOfItemInDB)
-    })
+  newEl.textContent = itemValue;
 
-    shoppingListEl.append(newEl)
+  newEl.addEventListener("click", function () {
+    let exactLocationOfItemInDB = ref(database, `shoppingList/${itemId}`);
+    remove(exactLocationOfItemInDB);
+  });
 
+  shoppingListEl.append(newEl);
 }
